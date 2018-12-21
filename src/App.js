492f9d82ee5e8485071bc9528ed809ab04039bd2
Git Learner <gitlearner3@yahoo.com>
2018-12-21 18:14:14 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Started using docker-compose. docker run. change one more time.
           </p>
           <a
             className="App-link"
